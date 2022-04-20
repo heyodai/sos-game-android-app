bb3e390ee160f124b4b7c5802c68a9418867a461
Odai Athamneh <heyodai@gmail.com>
2022-04-20 11:44:20 -0500

    Add vue (#2)
    * add vue package
    
    * add vuetify

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <!-- -->
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        test
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    // HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
