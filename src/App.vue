f6d3268dc34ae9c66e5443a671f8ef586658def0
Odai Athamneh <heyodai@gmail.com>
2022-04-20 18:50:52 -0500

    clean up app bar

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,18 @@
 <template>
   <v-app>
-    <NavDrawer />
-
-    
-      <AppBar />
-    
-
-    <!-- Sizes your content based upon application components -->
-    <!-- <v-main> -->
-      <!-- Provides the application the proper gutter -->
-      <!-- <v-container fluid> test </v-container>
-    </v-main> -->
+    <AppBar />
+    <v-main> </v-main>
   </v-app>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld";
 import AppBar from "./layout/AppBar.vue";
-import NavDrawer from "./layout/NavDrawer.vue";
 
 export default {
   name: "App",
 
   components: {
     AppBar,
-    NavDrawer,
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
