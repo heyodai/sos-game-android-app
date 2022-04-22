6f4feb4895ae74abe5e38217afe01af27b7cdccb
Odai Athamneh <heyodai@gmail.com>
2022-04-21 19:10:44 -0500

    add vue router (#4)
    * add screen components
    
    * add vue router
    
    * add router to vue app

---
+++ src/screens/HomeScreen.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>Home Screen</p>
+         <router-link to="/match">Go to Match</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HomeScreen",
+}
+</script>
---
+++ src/screens/MatchScreen.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>Match Screen</p>
+         <router-link to="/">Go to Home</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MatchScreen",
+}
+</script>
