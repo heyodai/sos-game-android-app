17bb573f2b235c2f9bcface7381686224cd065ba
Odai Athamneh <heyodai@gmail.com>
2022-04-23 13:14:09 -0500

    add basic player widget vue component

---
+++ src/components/PlayerWidget.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+    <v-icon>mdi-account</v-icon>
+    <strong>{{ this.name }}</strong>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "PlayerWidget",
+  data() {
+    return {
+      name: "name",
+    };
+  },
+  props: {
+    is_blue: Boolean,
+  },
+  created() {
+    this.name = this.is_blue ? "Blue Player" : "Red Player";
+  },
+};
+</script>
