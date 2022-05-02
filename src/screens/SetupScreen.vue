65d462150a94d93e1ef8cce5f3c07ce646e2e853
Odai Athamneh <heyodai@gmail.com>
2022-05-02 17:54:01 -0500

    add setup screen

--- src/screens/SetupScreen.vue
+++ src/screens/SetupScreen.vue
@@ -1,11 +1,29 @@
 <template>
-    <div>
-    </div>
+    <v-main>
+        <div id="wrapper">
+            <back-home-btn />
+            <h1>Match Setup</h1>
+        </div>
+    </v-main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import BackHomeBtn from '../components/BackHomeBtn.vue'
+
 export default Vue.extend({
     name: 'SetupScreen',
+    components: {
+        BackHomeBtn,
+    }
 })
 </script>
+
+<style scoped>
+    #wrapper {
+        margin: 1em;
+    }
+    #wrapper h1 {
+        margin: 0.5em 0;
+    }
+</style>
