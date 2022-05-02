90caf7961a71ac93fbe48b11c13146b07b13b1cb
Odai Athamneh <heyodai@gmail.com>
2022-05-02 17:51:38 -0500

    add credits screen content

--- src/screens/CreditsScreen.vue
+++ src/screens/CreditsScreen.vue
@@ -1,12 +1,37 @@
 <template>
-    <div>
-        credits
-    </div>
+    <v-main>
+        <div id="wrapper">
+            <back-home-btn />
+            <h1>Credits</h1>
+            <p>App by Odai Athamneh.</p>
+            <p>Many thanks to:</p>
+            <ul>
+                <li>Vue</li>
+                <li>Vuetify</li>
+                <li>Capacitor</li>
+                <li>GitHub</li>
+            </ul>
+        </div>
+    </v-main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import BackHomeBtn from '../components/BackHomeBtn.vue'
+
 export default Vue.extend({
     name: 'CreditsScreen',
+    components: {
+        BackHomeBtn,
+    }
 })
 </script>
+
+<style scoped>
+    #wrapper {
+        margin: 1em;
+    }
+    #wrapper h1 {
+        margin: 0.5em 0;
+    }
+</style>
