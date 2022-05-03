2bd3f0e2a44bea660f74be46f273a3bda60385c4
Odai Athamneh <heyodai@gmail.com>
2022-05-03 17:09:39 -0500

    add Board class

---
+++ src/classes/Board.ts
@@ -0,0 +1,26 @@
+import { Letter } from "@/enums"
+import Coordinates from "./Coordinates"
+import Square from "./Square"
+
+class Board {
+    public readonly MIN_SIZE: number = 3
+    public readonly MAX_SIZE: number = 5
+
+    public readonly rows: number
+    public readonly columns: number
+
+    public content: Square[][]
+
+    constructor(rows: number, columns: number) {
+        this.rows = rows
+        this.columns = columns
+
+        this.content = [];
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                let location = new Coordinates(i, j)
+                this.content[i][j] = new Square(location)
+            }
+        }
+    }
+}
