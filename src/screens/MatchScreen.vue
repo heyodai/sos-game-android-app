05ba3adb83b0a1d8e307128446ad6e525b5ff7df
Odai Athamneh <heyodai@gmail.com>
2022-05-03 18:37:27 -0500

    add basic match screen layout

--- src/screens/MatchScreen.vue
+++ src/screens/MatchScreen.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-app-bar color="primary" dense dark>
+    <v-app-bar color="primary" dense dark app>
       <v-toolbar-title id="app-bar-title">{{ this.title }}</v-toolbar-title>
 
       <v-spacer></v-spacer>
@@ -11,9 +11,7 @@
 
       <v-dialog v-model="exit_dialog" max-width="290">
         <v-card>
-          <v-card-title class="text-h5">
-            End the match?
-          </v-card-title>
+          <v-card-title class="text-h5"> End the match? </v-card-title>
 
           <v-card-actions>
             <v-spacer></v-spacer>
@@ -37,39 +35,135 @@
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
+
+    <v-main app>
+      <v-card>
+        <v-row>
+          <v-col>
+            <v-icon>
+              {{
+                this.attrs.red_player.is_human
+                  ? "mdi-account"
+                  : "mdi-desktop-classic"
+              }}
+            </v-icon>
+            <strong> Red Player </strong>
+          </v-col>
+
+          <v-col>
+            <v-switch
+              v-model="attrs.red_player.is_letter_s"
+              color="primary"
+              inset
+              :label="`${attrs.red_player.is_letter_s ? 'S' : 'O'}`"
+            ></v-switch>
+          </v-col>
+
+          <v-col>
+            {{ this.attrs.red_player.is_current_player ? "Your Turn" : "" }}
+          </v-col>
+        </v-row>
+      </v-card>
+
+      <v-card>
+        <table>
+          <tbody>
+            <tr v-for="i in attrs.size" :key="i">
+              <td v-for="i in attrs.size" :key="i">
+                <v-btn color="gray" height="50" width="50">
+                  <v-icon dark> mdi-minus </v-icon>
+                </v-btn>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </v-card>
+
+      <v-card>
+        <v-row>
+          <v-col>
+            <v-icon>
+              {{
+                this.attrs.blue_player.is_human
+                  ? "mdi-account"
+                  : "mdi-desktop-classic"
+              }}
+            </v-icon>
+            <strong> Blue Player </strong>
+          </v-col>
+
+          <v-col>
+            <v-switch
+              v-model="attrs.blue_player.is_letter_s"
+              color="primary"
+              inset
+              :label="`${attrs.blue_player.is_letter_s ? 'S' : 'O'}`"
+            ></v-switch>
+          </v-col>
+
+          <v-col>
+            {{ this.attrs.blue_player.is_current_player ? "Your Turn" : "" }}
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { GameMode, PlayerColor } from "@/enums";
+import Match from "../classes/Match";
 
 export default Vue.extend({
   name: "MatchScreen",
 
   data() {
     return {
+      attrs: {
+        red_player: {
+          is_human: this.$route.params.red == "human",
+          is_letter_s: false,
+          is_current_player: true,
+        },
+        blue_player: {
+          is_human: this.$route.params.blue == "human",
+          is_letter_s: false,
+          is_current_player: false,
+        },
+        mode:
+          this.$route.params.mode == "simple"
+            ? GameMode.SIMPLE
+            : GameMode.GENERAL,
+        size: parseInt(this.$route.params.size),
+      },
       start_time: 0,
       title: `${this.$route.params.mode} Match`,
       exit_dialog: false,
       stats_dialog: false,
-      is_red_human: (this.$route.params.red == "human"),
-      is_blue_human: (this.$route.params.blue == "human"),
+      match: new Match(GameMode.SIMPLE, 0, true, true),
     };
   },
 
   async created() {
-    this.start_time = Date.now()
+    this.start_time = Date.now();
+    this.match = this.start_match();
   },
 
   methods: {
-    start_match() {
-        return null
+    start_match(): Match {
+      return new Match(
+        this.attrs.mode,
+        this.attrs.size,
+        this.attrs.red_player.is_human,
+        this.attrs.blue_player.is_human
+      );
     },
     end_match() {
       console.log("It works!");
     },
     get_elapsed_time() {
-        return 0
+      return 0;
     },
   },
 });
