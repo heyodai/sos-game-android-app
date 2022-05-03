811200bc4115f6a908d90bcc001740877179d69c
Odai Athamneh <heyodai@gmail.com>
2022-05-03 17:16:47 -0500

    add Match class

---
+++ src/classes/Match.ts
@@ -0,0 +1,25 @@
+import { PlayerColor, GameMode, } from "@/enums"
+import Board from "./Board"
+import Player from "./Player"
+import MoveRecord from "../actions/MoveRecord"
+
+class Match {
+    private turn: number
+    // private history: MoveRecord[]
+
+    public red_player: Player
+    public blue_player: Player
+
+    public board: Board
+    public mode: GameMode
+
+    constructor(mode: GameMode, size: number, is_red_human: boolean, is_blue_human: boolean) {
+        this.turn = 0
+
+        this.board = new Board(size, size)
+        this.mode = mode
+
+        this.red_player = new Player(PlayerColor.RED, is_red_human)
+        this.blue_player = new Player(PlayerColor.BLUE, is_blue_human)
+    }
+}
