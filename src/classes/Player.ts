8164e18d48bb4384e9a8affe5b4b9749fd5c6c40
Odai Athamneh <heyodai@gmail.com>
2022-05-03 16:43:02 -0500

    define Player class

---
+++ src/classes/Player.ts
@@ -0,0 +1,13 @@
+import { PlayerColor } from "@/enums"
+
+class Player {
+    public readonly color: PlayerColor
+    public readonly is_human: boolean
+
+    constructor(color: PlayerColor, is_human: boolean) {
+        this.color = color
+        this.is_human = is_human
+    }   
+}
+
+export default Player
